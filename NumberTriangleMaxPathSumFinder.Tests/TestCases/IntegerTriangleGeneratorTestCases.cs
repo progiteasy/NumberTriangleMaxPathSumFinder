14c3e4192e37e0bb506af70180a73ea42276678d
Yaroslav Ryaguzov <progiteasy@gmail.com>
2022-02-20 15:56:15 +0300

    Added unit tests

---
+++ NumberTriangleMaxPathSumFinder.Tests/TestCases/IntegerTriangleGeneratorTestCases.cs
@@ -0,0 +1,49 @@
+﻿using NumberTriangleMaxPathSumFinder.Utilities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberTriangleMaxPathSumFinder.Tests.TestCases
+{
+    [TestFixture]
+    public class IntegerTriangleGeneratorTestCases
+    {
+        [Test]
+        public void TC1_WhenPassHeightNegativeValueToGenerateRandomlyMethod_ShouldGenerateArgumentException()
+        {
+            var triangleGenerator = new IntegerTriangleGenerator();
+            var triangleHeight = -1;
+            var randomGeneratorRange = ValueTuple.Create(-99, 100);
+
+            Assert.Throws<ArgumentException>(() => triangleGenerator.GenerateRandomly(triangleHeight, randomGeneratorRange));
+        }
+
+        [Test]
+        public void TC2_WhenPassRandomGeneratorRangeInvalidValueToGenerateRandomlyMethod_ShouldGenerateArgumentException()
+        {
+            var triangleGenerator = new IntegerTriangleGenerator();
+            var triangleHeight = 3;
+            var randomGeneratorRange = ValueTuple.Create(10, 1);
+
+            Assert.Throws<ArgumentException>(() => triangleGenerator.GenerateRandomly(triangleHeight, randomGeneratorRange));
+        }
+
+        [Test]
+        public void TC3_WhenCallGenerateRandomlyMethod_ShouldReturnValidIntegerTriangleObject()
+        {
+            var triangleGenerator = new IntegerTriangleGenerator();
+            var triangleHeight = 3;
+            var randomGeneratorRange = ValueTuple.Create(-10, 11);
+            var triangle = triangleGenerator.GenerateRandomly(triangleHeight, randomGeneratorRange);
+            var triangleNumbers = new List<int>();
+
+            for (var i = 0; i < triangle.Height; i++)
+                for (var j = 0; j < triangle[i].Length; j++)
+                    triangleNumbers.Add(triangle[i][j]);
+
+            Assert.AreEqual(triangle.Height, triangleHeight);
+            Assert.IsTrue(triangleNumbers.All(triangleNumber => triangleNumber >= randomGeneratorRange.Item1 && triangleNumber < randomGeneratorRange.Item2));
+        }
+    }
+}
---
+++ NumberTriangleMaxPathSumFinder.Tests/TestCases/IntegerTriangleTestCases.cs
@@ -0,0 +1,61 @@
+﻿using NumberTriangleMaxPathSumFinder.Models;
+using NUnit.Framework;
+using System;
+
+namespace NumberTriangleMaxPathSumFinder.Tests.TestCases
+{
+    [TestFixture]
+    public class IntegerTriangleTestCases
+    {
+        [Test]
+        public void TC1_WhenPassHeightNegativeValueToConstructor_ShouldGenerateArgumentException()
+        {
+            var triangleHeight = -1;
+
+            Assert.Throws<ArgumentException>(() => new IntegerTriangle(triangleHeight));
+        }
+
+        [Test]
+        public void TC2_WhenCallCalculateMaxPathSumMethod_ShouldReturn10()
+        {
+            var triangleHeight = 1;
+            var triangle = new IntegerTriangle(triangleHeight);
+
+            triangle[0][0] = 10;
+
+            Assert.AreEqual(triangle.CalculateMaxPathSum(), 10);
+        }
+
+        [Test]
+        public void TC3_WhenCallCalculateMaxPathSumMethod_ShouldReturn17()
+        {
+            var triangleHeight = 3;
+            var triangle = new IntegerTriangle(triangleHeight);
+
+            triangle[0][0] = 7;
+            triangle[1][0] = -2;
+            triangle[1][1] = 2;
+            triangle[2][0] = 3;
+            triangle[2][1] = 8;
+            triangle[2][2] = 7;
+
+            Assert.AreEqual(triangle.CalculateMaxPathSum(), 17);
+        }
+
+        [Test]
+        public void TC4_WhenCallCalculateMaxPathSumMethod_ShouldReturnMinus70()
+        {
+            var triangleHeight = 3;
+            var triangle = new IntegerTriangle(triangleHeight);
+
+            triangle[0][0] = -10;
+            triangle[1][0] = -20;
+            triangle[1][1] = -30;
+            triangle[2][0] = -50;
+            triangle[2][1] = -40;
+            triangle[2][2] = -50;
+
+            Assert.AreEqual(triangle.CalculateMaxPathSum(), -70);
+        }
+    }
+}
